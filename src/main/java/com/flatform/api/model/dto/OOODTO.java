88baf8ea41871b7ff3383d84e7c785dfaaf1610c
potato-CYH <mydream11123@naver.com>
2021-11-18 22:43:03 +0900

    H01. Initial Setting

---
+++ src/main/java/com/flatform/api/model/dto/OOODTO.java
@@ -0,0 +1,13 @@
+package com.flatform.api.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+//데이터 주고 받을 때 사용하기 위한 객체
+public class OOODTO {
+    private  String string;//DB Attribute name과 동일해야 함
+}
