3a89680982b4734c96f451881af3f0b581362e2b
potato-CYH <mydream11123@naver.com>
2021-11-25 10:16:48 +0900

    H17. Create : Create Server connecton Test class

---
+++ src/main/java/com/flatform/api/controller/ConnectionTestController.java
@@ -0,0 +1,20 @@
+//Test the Server connection!
+
+package com.flatform.api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class ConnectionTestController {
+
+    @ResponseBody
+    @GetMapping(value = "/api/test")
+    public String apiServerConnectonTest(){
+        return "Server Connecton OK!! Replied by ConnectonTestController Class.";
+    }
+}
